e00d695413e3d38b6d6d53d0070ab6f18148651a
Afred26 <schall.fredrik@gmail.com>
2025-04-11 14:10:16 +0000

    Zwischenstand

--- src/lecture/2025_04_11/find_lists/find_lists.cpp
+++ src/lecture/2025_04_11/find_lists/find_lists.cpp
@@ -6,3 +6,22 @@ int find_element(vector<int> list, int el) {
   auto iter = find(list.begin(), list.end(), el);
   return iter - list.begin();
 }
+
+int find_element(vector<string> names, string name) {
+  auto iter = find(names.begin(), names.end(), name);
+  return iter - names.begin();
+}
+
+int lookup_fee(string name, vector<string> names, vector<int> fees){
+if (names.size() != fees.size()) {
+  return -1;
+}
+
+  int pos = find_element(names, name);
+
+  if (pos == names.size()) {
+    return -1;
+  }
+
+  return fees[pos];
+}
--- src/lecture/2025_04_11/find_lists/find_lists_tests.cpp
+++ src/lecture/2025_04_11/find_lists/find_lists_tests.cpp
@@ -6,3 +6,14 @@ TEST_CASE("find_element", "[find]") {
   REQUIRE(find_element(std::vector{10, 20, 30, 40, 50}, 30) == 2);
   REQUIRE(find_element(std::vector{10, 20, 30, 40, 50}, 100) == 5);
 }
+
+TEST_CASE("find_element_string", "[find]") {
+  REQUIRE(find_element(std::vector{std::string{"Eiffelturm"},
+                                   std::string{"DHBW Mannheim"}},
+                       "Eiffelturm") == 0);
+}
+
+TEST_CASE("lookup_fee", "[find]") {
+  REQUIRE(lookup_fee("Eiffelturm",std::vector{std::string{"Eiffelturm"}, std::string{"DHBW Mannheim"}}, std::vector<int>{42, 23}) == 42);
+  REQUIRE(lookup_fee("Taj Mahal",std::vector{std::string{"Eiffelturm"}, std::string{"DHBW Mannheim"}}, std::vector<int>{42, 23}) == -1);
+}
